05c33504aff29d249ea851487f8b24447626f0e0
Ryan Boehm <ryan.w.boehm@gmail.com>
2022-05-16 01:14:16 -0400

    revamped tablet look

---
+++ js/cea_cable_impact.js
@@ -0,0 +1,15 @@
+
+jQuery(function($) {
+
+    $('.cea-industry-impact__row').flickity({
+      // options
+      cellAlign: 'left',
+      draggable: true,
+      imagesLoaded: true,
+      wrapAround: true,
+      watchCSS: true,
+      percentPosition: false,
+      prevNextButtons: false,
+    });
+
+});

8a23997d1d4d83e09ed5e71899d011000aa0b09b
Ryan Boehm <ryan.w.boehm@gmail.com>
2022-05-18 14:27:05 -0400

    swapped out lottie files library

--- js/cea_cable_impact.js
+++ js/cea_cable_impact.js
@@ -1,15 +1,75 @@
 
 jQuery(function($) {
 
-    $('.cea-industry-impact__row').flickity({
-      // options
-      cellAlign: 'left',
-      draggable: true,
-      imagesLoaded: true,
-      wrapAround: true,
-      watchCSS: true,
-      percentPosition: false,
-      prevNextButtons: false,
+    var lottie_gigabit_speeds = document.getElementById('lottie__gigabit_speeds');
+
+    lottie.loadAnimation({
+      container: lottie_gigabit_speeds,
+      renderer: 'svg',
+      loop: false,
+      autoplay: true,
+      path: '../lottie_files/Graphic-1-1.json'
     });
+
+    var lottie_access = document.getElementById('lottie__access');
+
+    lottie.loadAnimation({
+      container: lottie_access,
+      renderer: 'svg',
+      loop: false,
+      autoplay: true,
+      path: '../lottie_files/Graphic-1-2.json'
+    });
+
+    var lottie_broadband_programs = document.getElementById('lottie__broadband-programs');
+
+    lottie.loadAnimation({
+      container: lottie_broadband_programs,
+      renderer: 'svg',
+      loop: false,
+      autoplay: true,
+      path: '../lottie_files/Graphic-1-3.json'
+    });
+
+    var lottie_infrastructure_investment = document.getElementById('lottie__infrastructure-investment');
+
+    lottie.loadAnimation({
+      container: lottie_infrastructure_investment,
+      renderer: 'svg',
+      loop: false,
+      autoplay: true,
+      path: '../lottie_files/Graphic-2-1.json'
+    });
+
+    var lottie_miles_of_cable = document.getElementById('lottie__miles-of-cable');
+
+    lottie.loadAnimation({
+      container: lottie_miles_of_cable,
+      renderer: 'svg',
+      loop: false,
+      autoplay: true,
+      path: '../lottie_files/Graphic-2-2.json'
+    });
+
+    var lottie_cable_employees = document.getElementById('lottie__cable-employees');
+
+    lottie.loadAnimation({
+      container: lottie_cable_employees,
+      renderer: 'svg',
+      loop: false,
+      autoplay: true,
+      path: '../lottie_files/Graphic-3-1.json'
+    });
+
+    var lottie_cable_jobs = document.getElementById('lottie__cable-jobs');
+
+    lottie.loadAnimation({
+      container: lottie_cable_jobs,
+      renderer: 'svg',
+      loop: false,
+      autoplay: true,
+      path: '../lottie_files/Graphic-3-2.json'
+    });
+
 
 });
